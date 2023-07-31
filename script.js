c0581f3232c5302a1ecc1b67cae1d92635df2a19
MaryKonon <Mariakononenko99@gmail.com>
2023-07-31 17:35:14 +0300

    Added actual forecast

--- script.js
+++ script.js
@@ -41,6 +41,11 @@ function updateCityTitle(event) {
 }
 
 // Get city's temperature, precipitation, humidity, and wind
+function getForecast(coordinates) {
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat={coordinates.lat}&lon={coordinates.lon}&exclude={part}&appid={apiKey}&units=metric`;
+  let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+  axios.get(apiURL).then(updateCityData);
+}
 function updateCityData(response) {
   document.querySelector("#currentCity").innerHTML = response.data.name;
   let precipitation = document.querySelector("#currentPrecipitation");
@@ -48,11 +53,19 @@ function updateCityData(response) {
   let wind = document.querySelector("#currentWind");
   let temp = document.querySelector("#temperature");
   celsiusTemperature = response.data.main.temp;
+  let iconElement = document.querySelector("#icon");
 
   precipitation.innerHTML = `Precipitation: ${response.data.weather[0].main} `;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   temp.innerHTML = Math.round(celsiusTemperature);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 let searchForm = document.querySelector("#searchForm");
@@ -88,6 +101,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 // Current Location Button
 
 function searchLocation(position) {
@@ -104,5 +118,46 @@ function getCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
+  let date = new Date(response.data.daily[0].time * 1000);
+  let dayOfWeek = date.getDay();
+  let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let nameOfDayOfWeek = daysOfWeek[dayOfWeek];
+
+  // let forecastDailyHTML;
+  forecastDailyHTML = `<div class="row"><div class="col border border-3"></div>`;
+
+  // forecastDailyHTML = forecastDailyHTML + ` <div class="col-1"></div> `;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      console.log(index);
+      forecastDailyHTML =
+        forecastDailyHTML +
+        ` <div class="col border border-3">
+  <div class="col center" >${formatDay(forecastDay.time)}</div>
+  <img
+  src=${forecastDay.condition.icon_url}
+  alt=${forecastDay.condition.description}
+  id="forecast-heaven-icon"
+  width="65"
+  />
+  <div class="center">
+  <span class="max">${Math.round(
+    forecastDay.temperature.maximum
+  )}°</span> <span class="min">${Math.round(
+          forecastDay.temperature.minimum
+        )}°</span>
+  </div>
+  </div>`;
+    }
+  });
+
+  // console.log(forecastDailyHTML);
+  forecastDailyHTML = forecastDailyHTML + `</div>`;
+  forecastElement.innerHTML = forecastDailyHTML;
+}
 
 search();
